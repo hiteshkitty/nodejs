dff6a4cc67d0fcfef3ab95e3bc44470bd6915f13
Hitesh Sharma <jugardemailhai@gmail.com>
2020-08-24 00:14:05 +0530

    CRUD operations of mongoDB in separate js file

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-
+const operations = require('./operations.js');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
@@ -12,25 +12,51 @@ MongoClient.connect(url, (err, client) => {
 
     const db = client.db(dbname);
     const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
+    // collection.insertOne({"name": "Uthappizza", "description": "test"},
+    // (err, result) => {
+    //     assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    //     console.log("After Insert:\n");
+    //     console.log(result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err,null);
             
-            console.log("Found:\n");
-            console.log(docs);
+    //         console.log("Found:\n");
+    //         console.log(docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+    //         db.dropCollection("dishes", (err, result) => {
+    //             assert.equal(err,null);
 
-                client.close();
-            });
+    //             client.close();
+    //         });
+    //     });
+    // });
+
+    dishCollection = {"name" : "Raghav", "description":"sweet"};
+    operations.insertDocument(db, dishCollection, "dishes" , (result)=> {
+        console.log("inserted document: " + result.ops);
+
+        operations.findDocuments(db, "dishes", (result) => {
+            console.log("find documents: " + result);
+
+            operations.updateDocument(db, { name: "Raghav" },
+                    { description: "soo sweet" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
+
+                        operations.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+                            
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
+
+                                client.close();
+                            });
+                        });
+                    });
         });
-    });
 
+
+    });
 });
