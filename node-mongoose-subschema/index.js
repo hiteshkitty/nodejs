1763077baeaba490d9816b9939be3707f75c3349
Hitesh Sharma <jugardemailhai@gmail.com>
2020-08-24 19:26:35 +0530

    node mongoose subschema demo

---
+++ node-mongoose-subschema/index.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = "mongodb://localhost:27017/conFusion";
+
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("connected to db successfully");
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+        .then((dish) => {
+            console.log("going to create a dish " + dish.name);
+            return Dishes.findByIdAndUpdate(dish._id, { $set: { description: "Updated test" } }, {
+                new: true
+            }).exec();
+        })
+        .then((dish) => {
+            console.log(dish.description);
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+    
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
